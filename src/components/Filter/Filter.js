5ed1aef133d7ae438e2f55f02c990418256e29c4
Uri <uri555459@gmail.com>
2022-02-10 15:53:12 +0300

    Added class active filter button

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '../Button/Button'
 
 import './filter.scss'
 
 const Filter = ({ buttonHandler }) => {
 
+  useEffect(() => {
+    const buttons = document.querySelectorAll('.filter a')
+
+    function buttonActive() {
+      buttons.forEach(button => {
+        button.classList.remove('active')
+      })
+      this.classList.add('active')
+    }
+
+    buttons.forEach(button => {
+      button.addEventListener('click', buttonActive)
+    })
+  }, [])
   return (
     <div className='filter'>
-      <Button buttonHandler={buttonHandler} classes='button filter__btn' text='Все работы' dataAttr='all' />
+      <Button buttonHandler={buttonHandler} classes='button filter__btn active' text='Все работы' dataAttr='all' />
       <Button buttonHandler={buttonHandler} classes='button filter__btn' text='HTML CSS JS' dataAttr='html' />
       <Button buttonHandler={buttonHandler} classes='button filter__btn' text='Javascript' dataAttr='javascript' />
       <Button buttonHandler={buttonHandler} classes='button filter__btn' text='React' dataAttr='react' />
