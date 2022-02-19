51db96b060321d297c55212a5c9faac4a9f15b85
Uri <uri555459@gmail.com>
2022-02-19 11:09:50 +0300

    Added image preview

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,8 +3,8 @@ import Button from '../Button/Button';
 
 const Cart = ({ images = '', link = '', buttonText = '' }) => {
   return (
-    <div className="works__item">
-      <img src={images} className="works__item_img" alt="" />
+    <div className="works__item" style={{backgroundImage: `url(${images})`}}>
+      {/* <img src={images} className="works__item_img" alt="" /> */}
       <Button text={buttonText} link={link} target="_blank" />
     </div>
   );
