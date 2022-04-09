7726434709514b147fa5d2b8ddc096bfbfdc0309
Uri <uri555459@gmail.com>
2022-04-09 15:09:47 +0300

    swapped contact email and ckype

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,8 +5,9 @@ export const Contacts = () => {
         <div className='contacts-wrap'>
             <a href="https://wa.me/79892602382" target='_blank' rel="noreferrer"><img src="./images/social/whatsap.svg" alt="Whatsap" /></a>
             <a href="https://t.me/yury_web_dev" target='_blank' rel="noreferrer"><img src="./images/social/telegram.svg" alt="Telegram" /></a>
-            <a href="https://join.skype.com/invite/dscJMLOyPGzd" target='_blank' rel="noreferrer"><img src="./images/social/skype.svg" alt="Skype" /></a>
             <a href="mailto:uri5554@yandex.ru" rel="noreferrer"><img src="./images/social/email.svg" alt="Skype" /></a>
+            <a href="https://join.skype.com/invite/dscJMLOyPGzd" target='_blank' rel="noreferrer"><img src="./images/social/skype.svg" alt="Skype" /></a>
+           
         </div>
     )
 } 
