145e725acde9e2ea8b57944d6e2c04fa09f8fccf
Uri <uri555459@gmail.com>
2022-04-09 15:06:09 +0300

    Added contact mail

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -3,9 +3,10 @@ import './contacts.scss'
 export const Contacts = () => {
     return (
         <div className='contacts-wrap'>
-            <a href="https://wa.me/79892602382" target='_blank' rel="noreferrer"><img src="./images/whatsap.svg" alt="Whatsap" /></a>
-            <a href="https://t.me/yury_web_dev" target='_blank' rel="noreferrer"><img src="./images/telegram.svg" alt="Telegram" /></a>
-            <a href="https://join.skype.com/invite/dscJMLOyPGzd" target='_blank' rel="noreferrer"><img src="./images/skype.svg" alt="Skype" /></a>
+            <a href="https://wa.me/79892602382" target='_blank' rel="noreferrer"><img src="./images/social/whatsap.svg" alt="Whatsap" /></a>
+            <a href="https://t.me/yury_web_dev" target='_blank' rel="noreferrer"><img src="./images/social/telegram.svg" alt="Telegram" /></a>
+            <a href="https://join.skype.com/invite/dscJMLOyPGzd" target='_blank' rel="noreferrer"><img src="./images/social/skype.svg" alt="Skype" /></a>
+            <a href="mailto:uri5554@yandex.ru" rel="noreferrer"><img src="./images/social/email.svg" alt="Skype" /></a>
         </div>
     )
 } 
