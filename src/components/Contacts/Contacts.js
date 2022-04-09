ebe7bacf41da420312055c4f9b08ab053b571308
Uri <uri555459@gmail.com>
2022-04-09 14:53:27 +0300

    Added contact skype

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,6 +5,7 @@ export const Contacts = () => {
         <div className='contacts-wrap'>
             <a href="https://wa.me/79892602382" target='_blank' rel="noreferrer"><img src="./images/whatsap.svg" alt="Whatsap" /></a>
             <a href="https://t.me/yury_web_dev" target='_blank' rel="noreferrer"><img src="./images/telegram.svg" alt="Telegram" /></a>
+            <a href="https://join.skype.com/invite/dscJMLOyPGzd" target='_blank' rel="noreferrer"><img src="./images/skype.svg" alt="Skype" /></a>
         </div>
     )
 } 
