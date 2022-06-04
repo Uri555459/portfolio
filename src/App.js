7e02844ded14363752b6e84127d9f6519d0ccd31
Uri <uri555459@gmail.com>
2022-06-04 13:39:18 +0300

    Added tehnology git/github

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Header from './components/Header/Header'
-import TechnologyItem from './components/TechnologyItem/TechnologyItem'
-import Filter from './components/Filter/Filter'
-import Cart from './components/Cart/Cart'
-import Footer from './components/Footer/Footer'
-import { Contacts } from './components/Contacts/Contacts'
+import Header from "./components/Header/Header";
+import TechnologyItem from "./components/TechnologyItem/TechnologyItem";
+import Filter from "./components/Filter/Filter";
+import Cart from "./components/Cart/Cart";
+import Footer from "./components/Footer/Footer";
+import { Contacts } from "./components/Contacts/Contacts";
 
-import data from './DB.json'
+import data from "./DB.json";
 
 function App() {
-  const { cards, technologyItems, contacts } = data
-  const [workItems, setWorkItems] = useState(cards)
+  const { cards, technologyItems, contacts } = data;
+  const [workItems, setWorkItems] = useState(cards);
 
-  const buttonHandler = event => {
-    if (event.target.parentElement.classList.contains('button')) {
-      event.preventDefault()
+  const buttonHandler = (event) => {
+    if (event.target.parentElement.classList.contains("button")) {
+      event.preventDefault();
     }
-    const category = event.target.parentElement.dataset.category
-    const workItems = data.cards.filter(item => {
-      if (category === 'all') {
-        return item
+    const category = event.target.parentElement.dataset.category;
+    const workItems = data.cards.filter((item) => {
+      if (category === "all") {
+        return item;
       } else if (item.category === category) {
-        return item
+        return item;
       }
-      return null
-    })
-    return setWorkItems(workItems)
-  }
+      return null;
+    });
+    return setWorkItems(workItems);
+  };
 
   return (
     <div className="wrapper">
-
       <Header />
 
       <section className="about" id="about">
         <div className="container">
           <h3 className="section__title">Обо мне</h3>
           <p className="section__subtitle">
-            Целеустремленный, ответственный и исполнительный. Имея аналитический склад ума и логическое мышление - в
-            процессе верстки продумываю каждую деталь.
-            Верстка сайтов, интернет-магазинов и Landing Page (фиксированная, резиновая, адаптивная). Работаю
-            удаленно.
-            В работе использую SCSS, БЕМ, Gulp, Jquery, Javascript, Git, CMS Wordpress, React, NodeJs, Express, MongoDB, Redux.
+            Целеустремленный, ответственный и исполнительный. Имея аналитический
+            склад ума и логическое мышление - в процессе верстки продумываю
+            каждую деталь. Верстка сайтов, интернет-магазинов и Landing Page
+            (фиксированная, резиновая, адаптивная). Работаю удаленно. В работе
+            использую SCSS, БЕМ, Gulp, Jquery, Javascript, Git, CMS Wordpress,
+            React, NodeJs, Express, MongoDB, Redux.
           </p>
         </div>
       </section>
@@ -51,7 +51,10 @@ function App() {
         <div className="container">
           <h3 className="section__title">Технологии</h3>
           <div className="expert__cards">
-            {technologyItems.length && technologyItems.map(item => <TechnologyItem key={item.id} {...item} />)}
+            {technologyItems.length &&
+              technologyItems.map((item) => (
+                <TechnologyItem key={item.id} {...item} />
+              ))}
           </div>
         </div>
       </section>
@@ -61,15 +64,16 @@ function App() {
           <h3 className="section__title">Мои работы</h3>
           <Filter buttonHandler={buttonHandler} />
           <div className="works__inner">
-            {workItems.length && workItems.map(item => <Cart key={item.id} {...item} />)}
+            {workItems.length &&
+              workItems.map((item) => <Cart key={item.id} {...item} />)}
           </div>
         </div>
       </section>
 
       <Footer />
-      <Contacts contacts={contacts}/>
-    </div >
-  )
+      {/* <Contacts contacts={contacts}/> */}
+    </div>
+  );
 }
 
 export default App;
